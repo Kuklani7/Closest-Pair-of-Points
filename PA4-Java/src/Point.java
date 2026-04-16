5c034bc6f8c2a059d4f85a91d598861c4f1b65db
Kuklani7 <kuklanirt@gmail.com>
2026-04-16 01:21:34 -0400

    Updated Point.java
    Had to update the file due to the shift in packages such that the code still remains working and compilable.

--- PA4-Java/src/Point.java
+++ PA4-Java/src/Point.java
@@ -1,25 +1,24 @@
-package ub.cse.algo;
-
-import java.util.ArrayList;
-import java.util.List;
 import java.io.Serializable;
 
-public class Point implements Serializable{
+public class Point implements Serializable {
     public Integer _x;  // x coordinate
     public Integer _y;  // y coordinate
-    
-    Point(Integer x, Integer y) {
+
+    public Point(Integer x, Integer y) {
         _x = x;
         _y = y;
     }
-    
-    /**
-     * Used to compare if two Point match.
-     * @param compare The other Point that this is being compared to.
-     * @return true if they share the same x and y coordinate
-     */
-    public boolean equals(Point compare){
-        return (_x.equals(compare._x)) && (_y.equals(compare._y));
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Point compare = (Point) obj;
+        return _x.equals(compare._x) && _y.equals(compare._y);
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 * _x.hashCode() + _y.hashCode();
     }
 }
-
